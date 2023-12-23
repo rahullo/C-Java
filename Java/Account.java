948d0b5dddf2eff0c07093cdf4474def69716aa5
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-12-23 21:17:51 +0530

    Question no 3

---
+++ Java/Account.java
@@ -0,0 +1,28 @@
+public class Account {
+    int accountNum;
+    String accountHolderName;
+    int balance;
+    public Account(int accountNum,String accountHolderName, int balance){
+        this.accountNum = accountNum;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    int credit(int ammount){
+        this.balance += ammount;
+        return ammount;
+    }
+
+    int debit(int ammount){
+        this.balance -= ammount;
+        return ammount;
+    }
+
+    public static void main(String args[]){
+        Account ac1 = new Account(1234, "Rahul", 2000);
+
+        System.out.println(ac1.credit(200)+"RS Credit to your accout Number " + ac1.accountNum+ ". Available balance on your account is "+ac1.balance+"RS.");       
+        System.out.println(ac1.debit(100)+"RS Dedited from your accout Number " + ac1.accountNum+ ". Available balance on your account is "+ac1.balance+"RS.");
+
+    }
+}
