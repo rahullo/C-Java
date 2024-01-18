6aa0f1b1e4163b02e04ee3d92e5f676a8f39907c
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-18 18:47:08 +0530

    Implementing quicksort algo

--- C/LabAssignment/assignment14.c
+++ C/LabAssignment/assignment14.c
@@ -1,11 +1,46 @@
 #include<stdio.h>
 
+int partition(int array[], int low, int high){
+    int pivot = high;
+    int i = low-1;
+    int j;
+    for(j = low; j < high; j++){
+        if(array[j]<array[pivot]){
+            i++;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
 
-void quickSort(int array[], int size){
+        }
+    }
+    int temp2 = array[i+1];
+    array[i+1] = array[pivot];
+    array[pivot] = temp2;
+    return i+1;
+}
 
+void quickSort(int array[], int low, int high){
+    if(low < high){
+        int partitionPoint = partition(array,  low,  high);
+        quickSort(array, low, partitionPoint-1);
+        quickSort(array, partitionPoint+1, high);
+    }
+
+}
+
+void printingArray(int array[], int size){
+    int i;
+    for(i = 0; i < size; i++){
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
 }
 
 int main(){
     int array[] = {8, 9, 7, 5, 6, 3, 4, 2, 1, 0};
+
+    printingArray(array, 10);
+    quickSort(array, 0, 10);
+    printingArray(array, 10);
     
 }
