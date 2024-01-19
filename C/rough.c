a9d81a2afd4448143f1f002406ef77c00525c10a
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-19 23:38:42 +0530

    insertion sort

--- C/rough.c
+++ C/rough.c
@@ -1,34 +1,32 @@
 #include<stdio.h>
 
+// int partition(int array[], int low, int high){
+// 	printf("High: %d, array[%d]: %d\n", high, high, array[high]);
+// 	int pivot = high;
+// 	int key = low -1;
 
+// 	for(int i = low; i < high; i++){
+// 		if(array[i] < array[pivot]){
+// 			key++;
+// 			int temp1 = array[i];
+// 			array[i] = array[key];
+// 			array[key] = temp1;
 
-int partition(int array[], int low, int high){
-	printf("High: %d, array[%d]: %d\n", high, high, array[high]);
-	int pivot = high;
-	int key = low -1;
+// 		}
+// 	}
+// 	int temp2 = array[key+1];
+// 	array[key+1] = array[pivot];
+// 	array[pivot] = temp2;
+// 	return key+1;
+// }
 
-	for(int i = low; i < high; i++){
-		if(array[i] < array[pivot]){
-			key++;
-			int temp1 = array[i];
-			array[i] = array[key];
-			array[key] = temp1;
-
-		}
-	}
-	int temp2 = array[key+1];
-	array[key+1] = array[pivot];
-	array[pivot] = temp2;
-	return key+1;
-}
-
-int quickSort(int array[], int low, int high){
-	if(low < high){
-		int partitionIndex = partition(array, low, high);
-		quickSort(array, 0, partitionIndex-1);
-		quickSort(array, partitionIndex+1, high);
-	}
-}
+// int quickSort(int array[], int low, int high){
+// 	if(low < high){
+// 		int partitionIndex = partition(array, low, high);
+// 		quickSort(array, 0, partitionIndex-1);
+// 		quickSort(array, partitionIndex+1, high);
+// 	}
+// }
 
 int printArray(int array[], int size){
 	for(int i = 0; i < size; i++){
@@ -41,12 +39,30 @@ int printArray(int array[], int size){
 	printf("\n");
 }
 
+
+void insertionSort(int array[], int size){
+	for(int i = 1; i < size; i++){
+		int key = array[i];
+		int j = i-1;
+
+		while(array[j] > key&& j >= 0){
+			array[j+1] = array[j];
+			--j;
+		}
+
+		array[j+1] = key;
+	}
+}
+
+
+
+
 int main(){
 	int array[] = {6, 3, 7, 3, 2, 9, 0, 2, -1};
 
 	int size = sizeof(array)/sizeof(array[0]);
 
 	printArray(array, size);
-	quickSort(array, 0, size-1);
+	insertionSort(array,size);
 	printArray(array, size);
 }
