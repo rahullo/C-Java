6acdcc6ea5f8d07c2de354f46d4e9117ecf6de6b
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-20 11:06:06 +0530

    N - queen implementation

--- C/LabAssignment/assignment19.c
+++ C/LabAssignment/assignment19.c
@@ -12,8 +12,7 @@ bool isSafe(int board[N][N], int row, int col) {
         if (board[row][i])
             return false;
 
-    // Check upper diagonal on the left side
-    
+    // Check upper diagonal on the left side    
     for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
         if (board[i][j])
             return false;
