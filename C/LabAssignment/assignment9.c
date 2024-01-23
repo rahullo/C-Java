94d12e2b17736a51efdfb36a328682222c0a1575
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-23 18:50:01 +0530

    Linked List Implememtation

--- C/LabAssignment/assignment9.c
+++ C/LabAssignment/assignment9.c
@@ -1,24 +1,128 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// struct Node {
-//     int data;
-//     struct Node * next;
-// };
+// Define a Node structure
+struct Node {
+    int data;
+    struct Node *next;
+};
 
-int main(){
-    // struct Node node1;
-    // struct Node node2;
-    // struct Node node3;
-    // struct Node node4;
+// Function to create a new node with the given data
+struct Node* createNode(int data) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode != NULL) {
+        newNode->data = data;
+        newNode->next = NULL;
+    }
+    return newNode;
+}
 
-    // node1.data = 11;
-    // node1.next -> node2;
+// Function to insert a node at the beginning of the linked list
+struct Node* insertAtBeginning(struct Node *head, int data) {
+    struct Node *newNode = createNode(data);
+    if (newNode != NULL) {
+        newNode->next = head;
+        head = newNode;
+    }
+    return head;
+}
 
+// Function to insert a node at the end of the linked list
+struct Node* insertAtEnd(struct Node *head, int data) {
+    struct Node *newNode = createNode(data);
+    if (newNode != NULL) {
+        if (head == NULL) {
+            // If the list is empty, the new node becomes the head
+            head = newNode;
+        } else {
+            // Traverse to the end and append the new node
+            struct Node *current = head;
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    return head;
+}
 
-    int num = 10;
+// Function to delete a node with the given data from the linked list
+struct Node* deleteNode(struct Node *head, int data) {
+    struct Node *current = head;
+    struct Node *prev = NULL;
 
-    int * ptr = &num;
-    int* ptr2 = &num;
+    // Search for the node to delete
+    while (current != NULL && current->data != data) {
+        prev = current;
+        current = current->next;
+    }
 
-    printf("ptr: %d, ptr2: %d", ptr, ptr2);
+    // If the node is found, delete it
+    if (current != NULL) {
+        if (prev == NULL) {
+            // If the node is at the beginning
+            head = current->next;
+        } else {
+            // If the node is in the middle or end
+            prev->next = current->next;
+        }
+        free(current);
+    }
+
+    return head;
+}
+
+// Function to display the linked list
+void displayList(struct Node *head) {
+    struct Node *current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the memory allocated for the linked list
+void freeList(struct Node *head) {
+    struct Node *current = head;
+    struct Node *nextNode;
+
+    while (current != NULL) {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+}
+
+int main() {
+    struct Node *head = NULL;
+
+    // Insert nodes at the beginning
+    head = insertAtBeginning(head, 10);
+    head = insertAtBeginning(head, 20);
+    head = insertAtBeginning(head, 30);
+
+    // Display the linked list
+    printf("Linked List after insertions at the beginning:\n");
+    displayList(head);
+
+    // Insert nodes at the end
+    head = insertAtEnd(head, 40);
+    head = insertAtEnd(head, 50);
+
+    // Display the linked list
+    printf("Linked List after insertions at the end:\n");
+    displayList(head);
+
+    // Delete a node
+    head = deleteNode(head, 20);
+
+    // Display the linked list after deletion
+    printf("Linked List after deleting node with data 20:\n");
+    displayList(head);
+
+    // Free the memory allocated for the linked list
+    freeList(head);
+
+    return 0;
 }
