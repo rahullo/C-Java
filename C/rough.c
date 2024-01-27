8a867c17bd739b46a88b12603b5f2475e6b11300
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-27 08:26:33 +0530

    kanpsack revision

--- C/rough.c
+++ C/rough.c
@@ -56,19 +56,17 @@ void quickSort(int array[], int low, int high){
 }
 
 int knapsack(int wt[], int val[], int W, int N){
-	int dp[N+1][W+1];
+	int dp[N +1][W + 1];
 
-	for(int i = 0; i <=N; i++){
+	for(int i = 0; i <= N; i++){
 		for(int j = 0; j <= W; j++){
 			if(i == 0 || j == 0){
 				dp[i][j] = 0;
-			}else if (wt[i-1] > j)
-			{
+			}else if( wt[i-1] > j ){
 				dp[i][j] = dp[i-1][j];
 			}else{
-				dp[i][j] = maxi(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j]);
+				dp[i][j] = maxi(val[i-1] + dp[i-1][j - wt[i-1]] , dp[i-1][j]);
 			}
-			
 		}
 	}
 	return dp[N][W];
