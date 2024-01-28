cc63a8b3baea104c6350dd435409eb29c5996c8f
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-01-28 22:48:18 +0530

    threading

--- Java/threading.java
+++ Java/threading.java
@@ -3,10 +3,10 @@
 public class threading{
     public static void main(String args[]){
         int num = 80;
-        int division = 1;
         try{
 
-            division = num/0;
+            int division = num/0;
+            System.out.println(division);
         }catch(ArithmeticException e){
             System.out.println(e);
 
