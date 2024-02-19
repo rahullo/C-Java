eb5f5b71d450a026bd3cadb5d5da417fad6f8d61
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2024-02-19 20:06:11 +0530

    THreading

--- Java/MainSuper.java
+++ Java/MainSuper.java
@@ -1,27 +1,20 @@
+
 class Superclass {
-    int num = 10;
-    void display() {
-        System.out.println("Superclass method");
+    int num;
+    Superclass(int num) {
+        this.num = num;
     }
 }
 
 class Subclass extends Superclass {
-    int num = 20;
-    void display() {
-        super.display(); // Calling superclass method
-        System.out.println("Subclass method");
-    }
-    void print() {
-        System.out.println("Superclass num: " + super.num); // Accessing superclass variable
-        System.out.println("Subclass num: " + num);
+    Subclass(int num) {
+        super(num); // Calling superclass constructor
     }
 }
 
-public class MainSuper{
+public class MainSuper {
     public static void main(String[] args) {
-        Subclass obj = new Subclass();
-        obj.display(); // Output: Superclass method Subclass method
-        obj.print();   // Output: Superclass num: 10 Subclass num: 20
+        Subclass obj = new Subclass(10);
+        System.out.println("Value of num: " + obj.num); // Output: Value of num: 10
     }
 }
-
--- Java/threading.java
+++ Java/threading.java
@@ -1,16 +1,26 @@
 
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                Thread.sleep(1000); // Pause for 1 second
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
+    }
+}
 
-public class threading{
-    public static void main(String args[]){
-        int num = 80;
-        try{
+public class threading {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
 
-            int division = num/0;
-            System.out.println(division);
-        }catch(ArithmeticException e){
-            System.out.println(e);
+        thread1.setName("Thread 1");
+        thread2.setName("Thread 2");
 
-        }   
-
+        thread1.start();
+        thread2.start();
     }
 }
